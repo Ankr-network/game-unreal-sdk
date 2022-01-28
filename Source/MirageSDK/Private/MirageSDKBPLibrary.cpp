0959d03f5b2a237b32a49ff46a7b6d50d3a5ced7
Mücahit Büyükyılmaz <mucahit@yartu.io>
2022-01-28 23:35:41 +0300

    Add initial plugin structure

---
+++ Source/MirageSDK/Private/MirageSDKBPLibrary.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MirageSDKBPLibrary.h"
+#include "MirageSDK.h"
+
+UMirageSDKBPLibrary::UMirageSDKBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UMirageSDKBPLibrary::MirageSDKSampleFunction(float Param)
+{
+	return -1;
+}
